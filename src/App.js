4dd91866f2455e6e3c8a594f88f6192554453993
Richard Li <richardli125@gmail.com>
2019-11-24 16:51:22 -0500

    removed commented code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ const App = () => {
   return (
     <main className="Application">
       <section className="Counters">
-        {/* <Counter /> */}
         <CounterHooks max={15} step={5} />
       </section>
     </main>
